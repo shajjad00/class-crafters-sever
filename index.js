7ca55d361b48a465d64861e8dd773675a727f2f2
shajjad00 <shajjadph@gmail.com>
2023-11-29 00:59:51 +0600

    added payment method

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
-
 //crypto
 
 // const crypto = require("crypto");
@@ -39,6 +39,18 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    //payment related
+
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "USD",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
     // admin  related api
 
     app.post("/users/", async (req, res) => {
